34053371d83442b4ac913f192629e295f57ed154
Andrew Burdyug <buran83@gmail.com>
2025-04-11 21:31:51 +0300

    Add upload

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/1buran/ssh-run
 go 1.24.1
 
 require (
+	github.com/pkg/sftp v1.13.9
 	golang.org/x/crypto v0.37.0
 	golang.org/x/term v0.31.0
 )
 
-require golang.org/x/sys v0.32.0 // indirect
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"slices"
 	"strings"
 	"sync"
 	"syscall"
 	"time"
 
+	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/term"
 )
@@ -27,6 +30,25 @@ func (t *Timeout) Set(value string) (err error) {
 	return
 }
 
+type Upload struct {
+	src, dst string
+}
+
+func (u Upload) String() string { return fmt.Sprintf("%s:%s", u.src, u.dst) }
+func (u *Upload) Set(value string) error {
+	parts := strings.Split(value, ":")
+	if len(parts) < 2 {
+		return errors.New("malformed upload path, use: /local/path:/host/path")
+	}
+	u.src, u.dst = parts[0], parts[1]
+	if len(u.src) == 0 || len(u.dst) == 0 {
+		return fmt.Errorf("path cannot be empty, parsed local path: %q, parsed host path: %q", u.src, u.dst)
+	}
+	f, err := os.Open(u.src)
+	defer f.Close()
+	return err
+}
+
 func main() {
 	var (
 		cmd, user, privKey string
@@ -35,6 +57,8 @@ func main() {
 		timeout = Timeout{10 * time.Second}
 
 		readHostsFromFile string
+		upload            Upload
+		params            []string
 	)
 
 	flag.StringVar(&cmd, "c", "w", "command")
@@ -43,8 +67,17 @@ func main() {
 	flag.StringVar(&privKey, "i", "", "private key path")
 	flag.Var(&timeout, "t", "timeout")
 	flag.StringVar(&readHostsFromFile, "f", "", "read hosts from file")
+	flag.Var(&upload, "upload", "upload file to host (format: /local/path:/host/path)")
 
 	flag.Parse()
+
+	flag.Visit(func(f *flag.Flag) {
+		params = append(params, f.Name)
+	})
+
+	if slices.Contains(params, "c") && slices.Contains(params, "upload") {
+		log.Fatal("command and upload are mutually exclusive parameters")
+	}
 
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
@@ -144,26 +177,55 @@ func main() {
 				}
 			}()
 
-			// var cmdOutput strings.Builder // prevent mix of output of goroutines
-			session, err := client.NewSession()
-			if err != nil {
-				logger.Println("Failed to create session: ", err)
-				return
-			}
-			defer func() {
-				if err := session.Close(); err != nil && err != io.EOF {
+			if slices.Contains(params, "c") {
+				// var cmdOutput strings.Builder // prevent mix of output of goroutines
+				session, err := client.NewSession()
+				if err != nil {
+					logger.Println("Failed to create session: ", err)
+					return
+				}
+				defer func() {
+					if err := session.Close(); err != nil && err != io.EOF {
+						logger.Println(err)
+						return
+					}
+				}()
+
+				// todo: session.Stdin = os.Stdin
+				session.Stdout = &outBuf
+				session.Stderr = &errBuf
+
+				if err = session.Run(cmd); err != nil {
 					logger.Println(err)
 					return
 				}
-			}()
+			}
+			if slices.Contains(params, "upload") {
+				sftpClient, err := sftp.NewClient(client)
+				if err != nil {
+					logger.Println(err)
+					return
+				}
+				defer sftpClient.Close()
 
-			// todo: session.Stdin = os.Stdin
-			session.Stdout = &outBuf
-			session.Stderr = &errBuf
+				dstFile, err := sftpClient.Create(upload.dst)
+				if err != nil {
+					logger.Println(err)
+					return
+				}
 
-			if err = session.Run(cmd); err != nil {
-				logger.Println(err)
-				return
+				srcFile, err := os.Open(upload.src)
+				if err != nil {
+					logger.Println(err)
+					return
+				}
+
+				n, err := io.Copy(dstFile, srcFile)
+				if err != nil {
+					logger.Println(err)
+					return
+				}
+				fmt.Fprintf(&outBuf, "%d bytes written to file %s", n, upload.dst)
 			}
 		}()
 	}
